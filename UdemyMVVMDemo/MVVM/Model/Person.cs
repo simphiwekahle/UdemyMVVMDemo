82ee2725b349d297eb2fea88b3ad48be569200ca
Sphephelo <sphephelon@hollywoodbets.net>
2024-08-30 12:58:44 +0200

    Adding Other Model Variables - Updating Person model - Updating the ViewModel - Binding the added variables

--- UdemyMVVMDemo/MVVM/Model/Person.cs
+++ UdemyMVVMDemo/MVVM/Model/Person.cs
@@ -10,4 +10,8 @@ public class Person
 {
     public string Name { get; set; } = string.Empty;
     public int Age { get; set; }
+    public bool Married { get; set; }
+    public DateTime BirthDate { get; set; }
+    public int Weight { get; set; }
+    public TimeSpan LunchTime { get; set; }
 }
--- UdemyMVVMDemo/MVVM/ViewModel/PersonViewModel.cs
+++ UdemyMVVMDemo/MVVM/ViewModel/PersonViewModel.cs
@@ -10,7 +10,11 @@ public class PersonViewModel
         Person = new Person
         {
             Name = "Sphephelo",
-            Age = 27
+            Age = 27,
+            Married = false,
+            BirthDate = new DateTime(1997, 07, 05),
+            Weight = 70,
+            LunchTime = new TimeSpan(10, 0, 0)
         };
     }
 }
