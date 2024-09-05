a1f764e485d8c2a672b3de192565350c2d3fcd5f
Sphephelo <sphephelon@hollywoodbets.net>
2024-09-05 10:57:15 +0200

    PeopleViewModel Implementation
    Setting up the PeopleViewModel as the BindingContext

--- UdemyMVVMDemo/MVVM/View/PeopleView.xaml.cs
+++ UdemyMVVMDemo/MVVM/View/PeopleView.xaml.cs
@@ -1,3 +1,5 @@
+using UdemyMVVMDemo.MVVM.ViewModel;
+
 namespace UdemyMVVMDemo.MVVM.View;
 
 public partial class PeopleView : ContentPage
@@ -5,5 +7,7 @@ public partial class PeopleView : ContentPage
 	public PeopleView()
 	{
 		InitializeComponent();
-	}
+		BindingContext = new PeopleViewModel();
+
+    }
 }
---
+++ UdemyMVVMDemo/MVVM/ViewModel/PeopleViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace UdemyMVVMDemo.MVVM.ViewModel;
+
+public class PeopleViewModel
+{
+    public List<string> People { get; set; }
+
+    public PeopleViewModel()
+    {
+        People = new List<string>
+        {
+            "Sphephelo",
+            "Melusi",
+            "Endinako",
+            "Siyabonga",
+            "Clive"
+        };
+    }
+}
